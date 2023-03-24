8d69cd6ad6a9c7dba836db2b38a74b0a7c35c788
brian-stuczynski-ci <116681696+brian-stuczynski-ci@users.noreply.github.com>
2023-03-24 13:43:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Blueberry Oatmeal')
+
+streamlit.text('Bacon Egg and Cheese')
